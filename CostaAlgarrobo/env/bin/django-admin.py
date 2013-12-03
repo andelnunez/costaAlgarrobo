dda3482f61ffff134dc53c69860b8530a582e3ef
Andres E. Azpurua <andresazp@gmail.com>
2013-12-03 18:53:27 -0430

    .gitignore is now working

--- CostaAlgarrobo/env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/nano/code/turpial/costaAlgarrobo/sistema/costaAlgarrobo/CostaAlgarrobo/env/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
